450f469ae8164763a8e397b2b72bde792b07e8fc
Arunsh93 <Arunhosur93@gmail.com>
2021-10-01 12:11:30 +0530

    Maximum number between 3 Integers

---
+++ MaximumNumberUsingGenerics/MainMethodClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MaximumNumberUsingGenerics
+{
+    class MainMethodClass
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Find Maximum Number");
+            int output = UC1_MaximumNumber.MaximumIntegerNumber(22, 33, 44);
+            Console.WriteLine(output);
+        }
+    }
+}
