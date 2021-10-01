e2d9b28d27783eb1b80ea8e6a949af281677a520
Arunsh93 <Arunhosur93@gmail.com>
2021-10-01 12:39:30 +0530

    Maximum double number between 3 double numbers

--- MaximumNumberUsingGenerics/MainMethodClass.cs
+++ MaximumNumberUsingGenerics/MainMethodClass.cs
@@ -9,6 +9,9 @@ namespace MaximumNumberUsingGenerics
             Console.WriteLine("Find Maximum Number");
             int output = UC1_MaximumNumber.MaximumIntegerNumber(22, 33, 44);
             Console.WriteLine(output);
+
+            double doubleOutput = UC1_MaximumNumber.MaximumIntegerNumber(12.7, 20.47, 80.56);
+            Console.WriteLine(doubleOutput);
         }
     }
 }
